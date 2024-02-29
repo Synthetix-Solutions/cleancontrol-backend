8d8c131c91d56aaa1deb539835b479e34e290b2e
Futagirl <anime@incest.rocks>
2024-02-29 09:57:55 +0100

    more cors

--- src/Program.cs
+++ src/Program.cs
@@ -16,7 +16,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services
-	   .AddCors()
+	   .AddCors(o => o.AddPolicy("lala", p => p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()))
 	   .AddProblemDetails()
 	   .AddEndpointsApiExplorer()
 	   .AddSwaggerGen(
